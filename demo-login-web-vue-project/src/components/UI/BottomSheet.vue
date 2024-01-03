a7098e68a3e8b4074aeed5d3580b67c2832804d0
jhg1126q <98995858+jhg1126q@users.noreply.github.com>
2024-01-03 15:04:52 +0900

    🎨vuetify 적용
    vuetify 적용 올림
    간단한 bottom spread sheet 작성 후 테스트
    기본 골자 적용 중이라 master에 바로 반영

---
+++ demo-login-web-vue-project/src/components/UI/BottomSheet.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data: () => ({
+    sheet: false
+  })
+}
+</script>
+<template>
+  <div class="text-center">
+    <v-btn size="x-large" text="열기" @click="sheet = !sheet"></v-btn>
+
+    <v-bottom-sheet v-model="sheet">
+      <v-card class="text-center" height="200">
+        <v-card-text>
+          <v-btn variant="text" @click="sheet = !sheet"> 닫기 </v-btn>
+
+          <br />
+          <br />
+
+          <div>클릭시 나오게 되는 파트입니다.</div>
+        </v-card-text>
+      </v-card>
+    </v-bottom-sheet>
+  </div>
+</template>
