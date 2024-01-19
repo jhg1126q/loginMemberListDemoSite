adebf6ba4f93237e2edb85b4dc0b20186074e9f1
jhg1126q <98995858+jhg1126q@users.noreply.github.com>
2024-01-19 17:31:45 +0900

    🎨화면 추가 및 wireFrame 추가
    wireFrame을 추가
    화면 구조 잡고 본격적으로 시작할 준비

--- demo-login-web-vue-project/src/views/MainView.vue
+++ demo-login-web-vue-project/src/views/MainView.vue
@@ -1,31 +1,123 @@
+<template>
+  <v-app id="inspire">
+    <v-app-bar flat>
+      <v-container class="mx-auto d-flex align-center justify-center">
+        <v-avatar class="me-4" color="grey-darken-1" size="32" @click="drawer = !drawer"></v-avatar>
+
+        <v-btn v-for="link in linkData" :key="link" :text="link.name" variant="text"></v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-text-field
+          density="compact"
+          flat
+          hide-details
+          label="Search"
+          rounded="lg"
+          single-line
+          variant="solo-filled"
+        ></v-text-field>
+      </v-container>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" temporary>
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row class="d-flex" justify="center">
+          <v-menu v-model="showMenu" absolute offset-y style="max-width: 600px">
+            <template v-slot:activator="{ props }">
+              <v-card
+                class="portrait"
+                image="https://cdn.vuetifyjs.com/images/cards/girl.jpg"
+                height="300"
+                width="600"
+                v-bind="props"
+                @contextmenu="show"
+              ></v-card>
+            </template>
+
+            <v-list>
+              <v-list-item v-for="(item, index) in items" :key="index">
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-row>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                <v-list-item v-for="n in 5" :key="n" link :title="`List Item ${n}`"></v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item color="grey-lighten-4" link title="Refresh"></v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet min-height="70vh" rounded="lg" style="display: flex">
+              <!--  -->
+
+              <Game ref="gameRef" @logtr="testfunction" />
+              <GameLog :log="testList"></GameLog>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
 <script setup>
-import { ref } from 'vue'
+import { nextTick, ref } from 'vue'
+import Game from '../components/Game/Game.vue'
+import GameLog from '../components/Game/Log/GameLog.vue'
 
-const test = ref('')
+const links = ['Dashboard', 'Messages', 'Profile', 'Updates']
+const linkData = [
+  { link: '/', name: 'MainBoard' },
+  { link: '/dungeon', name: 'Dungeon' },
+  { link: '/shop', name: 'Shop' },
+  { link: '/profile', name: 'MyPage' }
+]
 
-const fn_test = () => {
-  console.log('test')
+const showMenu = ref(false)
+const items = [
+  { title: 'Click Me' },
+  { title: 'Click Me' },
+  { title: 'Click Me' },
+  { title: 'Click Me 2' }
+]
+const x = ref(0)
+const y = ref(0)
+function show(e) {
+  e.preventDefault()
+  showMenu.value = false
+  x.value = e.clientX
+  y.value = e.clientY
+  nextTick(() => {
+    showMenu.value = true
+  })
+}
+
+const drawer = ref(false)
+const gameRef = ref(null)
+const testList = ref([])
+const testfunction = () => {
+  // 데이터 바인딩이 아니라 그저 데이터를 후처리로 퍼 넘겨주는 형식입니다.
+  testList.value = gameRef.value.playLog
 }
 </script>
 
-<template>
-  <v-app-bar :elevation="0" rounded>
-    <v-app-bar-title>메인 페이지</v-app-bar-title>
-    <template v-slot:append>
-      <v-btn icon="mdi-heart"></v-btn>
-
-      <v-btn icon="mdi-magnify"></v-btn>
-
-      <v-btn icon="mdi-dots-vertical"></v-btn>
-    </template>
-  </v-app-bar>
-
-  <v-row>
-    <v-col cols="12">
-      <h1>Welcome to Our Website</h1>
-      <p>fuck</p>
-    </v-col>
-  </v-row>
-
-  <span>&copy; 2024</span>
-</template>
+<style scoped>
+.portrait.v-card {
+  margin: 0 auto;
+  max-width: 600px;
+  width: 100%;
+}
+</style>
